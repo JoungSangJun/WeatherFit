684a2d1db712819c83f785861b67465eb82b81e8
JoungSangJun <gogumi4502@nate.com>
2023-05-08 15:58:03 +0900

    feat : gitIgnore 해제

--- app/src/main/java/com/example/weatherfit/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/weatherfit/ui/home/HomeScreen.kt
@@ -1,14 +1,25 @@
 package com.example.weatherfit.ui.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun HomeScreen() {
-
-    Button(onClick = {  }) {
-        Text("homeScreen")
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Home Screen",
+            fontSize = 24.sp
+        )
     }
 }
